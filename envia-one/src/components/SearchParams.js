340d9be1c5a267b555943c02324f48aae1950b87
toshiroto <chtopete@gmail.com>
2022-06-23 16:11:58 +0900

    Product List

--- envia-one/src/components/SearchParams.js
+++ envia-one/src/components/SearchParams.js
@@ -2,13 +2,22 @@ import { useState, useEffect } from "react";
 import Woman from "./Woman";
 import axios from "axios";
 
- const API_URL = "http://localhost:3000/api/v1/women"
+const API_URL = "http://localhost:3000/api/v1/women";
 
- function getAPIData() {
-  return axios.get(API_URL).then((response) => response.data)
- }
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data);
+}
 
-const BUSINESS = ["Cafeteria", "Tapetes", "Bordados", "Bolsas", "Comedor",  "Miscelanea", "Tortillas", "Pollos"]
+const BUSINESS = [
+  "Cafeteria",
+  "Tapetes",
+  "Bordados",
+  "Bolsas",
+  "Comedor",
+  "Miscelanea",
+  "Tortillas",
+  "Pollos",
+];
 
 const SearchParams = () => {
   const [town, setTown] = useState("");
@@ -16,7 +25,6 @@ const SearchParams = () => {
   const [product, setProduct] = useState("");
   const [women, setWomen] = useState([]);
   const products = [];
-
 
   useEffect(() => {
     let mounted = true;
@@ -26,74 +34,91 @@ const SearchParams = () => {
       }
     });
 
-    return () => (mounted = false)
+    return () => (mounted = false);
   }, []);
 
-//   useEffect(() => {
-//     requestWomen();
-//   }, []);  // eslint-disable-line react-hooks/exhaustive-deps
+  //   useEffect(() => {
+  //     requestWomen();
+  //   }, []);  // eslint-disable-line react-hooks/exhaustive-deps
 
-//   async function requestWomen() {
-//     const res = await fetch (
-//       `http://localhost:3000/api/v1/women?business=${business}&town=${town}&product=${product}`
-//     );
-//   const json = await res.json();
+  //   async function requestWomen() {
+  //     const res = await fetch (
+  //       `http://localhost:3000/api/v1/women?business=${business}&town=${town}&product=${product}`
+  //     );
+  //   const json = await res.json();
 
-//   setWomen(json.women);
-// }
-
+  //   setWomen(json.women);
+  // }
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="town">
           Town
-          <input id="town" value={town} placeholder="town" onChange={(e) => setTown(e.target.value)} />
+          <input
+            id="town"
+            value={town}
+            placeholder="town"
+            onChange={(e) => setTown(e.target.value)}
+          />
         </label>
         <label htmlFor="business">
           Business
-          <select name="business" id="business" value={business} onChange={(e) => {
-            setBusiness(e.target.value);
-            setProduct("");
-          }}
-          onBlur={(e) => {
-            setBusiness(e.target.value);
-            setProduct("");
-          }}
+          <select
+            name="business"
+            id="business"
+            value={business}
+            onChange={(e) => {
+              setBusiness(e.target.value);
+              setProduct("");
+            }}
+            onBlur={(e) => {
+              setBusiness(e.target.value);
+              setProduct("");
+            }}
           >
             <option />
             {BUSINESS.map((business) => (
-      <option key={business} value={business}>
-        {business}
-              </option >
+              <option key={business} value={business}>
+                {business}
+              </option>
             ))}
           </select>
         </label>
         <label htmlFor="product">
           Social
-          <select disabled={!products.length} name="product" id="product" value={product} onChange={(e) => {
-            setProduct(e.target.value);
-          }}
-          onBlur={(e) => {
-            setProduct(e.target.value);
-          }}
+          <select
+            disabled={!products.length}
+            name="product"
+            id="product"
+            value={product}
+            onChange={(e) => {
+              setProduct(e.target.value);
+            }}
+            onBlur={(e) => {
+              setProduct(e.target.value);
+            }}
           >
             <option />
             {products.map((product) => (
-      <option key={product} value={product}>
-        {product}
-              </option >
+              <option key={product} value={product}>
+                {product}
+              </option>
             ))}
           </select>
         </label>
         <button>Submit</button>
       </form>
-      {
-  women.map((woman) => (
-    <Woman name={woman.name} business={woman.business} product={woman.product} key={woman.id} />
-  ))}
+      {women.map((woman) => (
+        <Woman
+          name={woman.name}
+          business={woman.business}
+          product={woman.product}
+          key={woman.id}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
---
+++ envia-one/src/components/useProductList.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const localCache = {};
+
+export default function useProductList(business) {
+  const [productList, setProductList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if(!business) {
+      setProductList([]);
+    } else if (localCache[business]) {
+    setProductList(localCache[business])
+  } else {
+    requestProductList();
+  }
+
+  async function requestProductList() {
+    setProductList([]);
+    setStatus("loading");
+
+    const res = await fetch(`http://localhost:3000/api/v1/women/products?business=${business}`);
+
+    const json = await res.json();
+    localCache[business] = json.products || [];
+    setProductList(localCache[business]);
+    setStatus("loaded");
+
+  }
+  })
+}
